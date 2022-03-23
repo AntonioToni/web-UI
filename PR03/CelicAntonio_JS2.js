a2ecaaa54d93745db922afd2a4c302391b1b3496
Antonio <antonio.gaming10@gmail.com>
2022-03-23 02:43:36 +0100

    Task 2

--- PR03/CelicAntonio_JS2.js
+++ PR03/CelicAntonio_JS2.js
@@ -7,3 +7,31 @@ function addFL(){
 }
 
 console.log(addFL(3, 4, 7, 3));
+
+//2
+const arr = [1, 3, -5 , 2 ,4, -6, -2, -4, -1, 5]
+
+function func(arr, checkpos, checkdivisable, y){
+    for (let i = 0; i < arr.length; i++) {
+        if (checkpos(arr[i]) == true && checkdivisable(arr[i], y) == true) {
+            console.log(arr[i]);
+        }
+    }
+}
+
+function checkpos(x){
+    if (x > 0) {
+        return true;
+    }
+    return false;
+}
+
+function checkdivisable(x, y){
+    if (x % y === 0){
+        return true;
+    }
+    return false;
+}
+func(arr, checkpos, checkdivisable, 3);
+
+//
