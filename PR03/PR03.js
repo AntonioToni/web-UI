ac523c312b9cb8d39ed72eb13f4303a7304cbcbe
Antonio <antonio.gaming10@gmail.com>
2022-03-23 03:29:16 +0100

    Task 3

--- PR03/PR03.js
+++ PR03/PR03.js
@@ -1,5 +1,5 @@
 //1
-function addFL(){
+function addFL() {
     var result = 0;
     result += arguments[0];
     result += arguments[arguments.length-1];
@@ -11,7 +11,7 @@ console.log(addFL(3, 4, 7, 3));
 //2
 const arr = [1, 3, -5 , 2 ,4, -6, -2, -4, -1, 5]
 
-function func(arr, checkpos, checkdivisable, y){
+function func(arr, checkpos, checkdivisable, y) {
     for (let i = 0; i < arr.length; i++) {
         if (checkpos(arr[i]) == true && checkdivisable(arr[i], y) == true) {
             console.log(arr[i]);
@@ -19,14 +19,14 @@ function func(arr, checkpos, checkdivisable, y){
     }
 }
 
-function checkpos(x){
+function checkpos(x) {
     if (x > 0) {
         return true;
     }
     return false;
 }
 
-function checkdivisable(x, y){
+function checkdivisable(x, y) {
     if (x % y === 0){
         return true;
     }
@@ -34,4 +34,24 @@ function checkdivisable(x, y){
 }
 func(arr, checkpos, checkdivisable, 3);
 
-//
+//3
+function sum(UserEnter, amount, add){
+    let total = 0;
+    for (let i = 0; i < amount; i++) {
+        total = add(total,UserEnter());
+    }
+    return total;
+}
+
+function UserEnter() {
+    let x = prompt("Enter a number:");
+    let xx = parseInt(x);
+    return xx;
+}
+
+function add(total, x) {
+    total += x;
+    return total;
+}
+
+console.log(sum(UserEnter, 4, add));
