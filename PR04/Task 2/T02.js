d92b9c0ff5afad8b51839a385241f4fc00a7e964
Antonio <antonio.gaming10@gmail.com>
2022-03-30 11:47:28 +0200

    Fixed

--- PR04/Task 2/T02.js
+++ PR04/Task 2/T02.js
@@ -1,6 +1,10 @@
-function res(name, points, totalpoints) {
-    let x = points / totalpoints * 100;
-    console.log("Iz kolegija", name, "ostvarili ste",x.toFixed(2),"%");
+function res(name) {
+    return points => {
+        return totalpoints => {
+            let x = points / totalpoints * 100;
+            console.log("Iz kolegija", name, "ostvarili ste",x.toFixed(2),"%");
+        }
+    }
 }
 
-res(("MUP/PWKS"),(25),(35));
+res("MUP/PWKS")(25)(35);
