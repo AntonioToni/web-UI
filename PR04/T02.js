7b204572f2e11386c7fbef070aa7926c9aea5b15
Antonio <antonio.gaming10@gmail.com>
2022-03-30 03:46:59 +0200

    Init commit

---
+++ PR04/T02.js
@@ -0,0 +1,6 @@
+function res(name, points, totalpoints) {
+    let x = points / totalpoints * 100;
+    console.log("Iz kolegija", name, "ostvarili ste",x.toFixed(2),"%");
+}
+
+res("MUP/PWKS",25,35);
