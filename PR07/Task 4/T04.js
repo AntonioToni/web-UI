695e1d53c70be5500e37af1b4c1603862014233a
Antonio <antonio.gaming10@gmail.com>
2022-05-09 21:40:12 +0200

    Task 04

---
+++ PR07/Task 4/T04.js
@@ -0,0 +1,26 @@
+function specialReverse(string, char) {
+    let word = string.split(" ");
+    let newstr = "";
+    for (let i = 0; i < word.length; i++) {
+        let b = word[i][0]; //get first letter
+        if (b === char) {
+            let e = word.indexOf(word[i]);
+            let c = "";
+            for (let j = word[i].length - 1; j >= 0; j--) { //reverse
+                c += word[i][j];
+        }
+        c += "";
+        word.splice(e, 1, c); //at index 1, delete 1, insert c
+        }
+    }
+    for (let i = 0; i < word.length; i++) {
+        newstr += word[i] + " ";
+    }
+    console.log(newstr);
+}
+
+specialReverse("word searches are super fun", "s");
+
+specialReverse("first man to walk on the moon", "m");
+
+specialReverse("peter piper picked pickled peppers", "p");
