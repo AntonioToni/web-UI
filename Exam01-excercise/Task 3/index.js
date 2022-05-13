b5d122342b40f261deb7df9749e4758ec034932d
Antonio <antonio.gaming10@gmail.com>
2022-05-13 14:14:41 +0200

    Task 3

---
+++ Exam01-excercise/Task 3/index.js
@@ -0,0 +1,5 @@
+/*
+.map() holds key-value pairs, it's good to use it while making some sort of array which will hold certain value for certain key
+.filter() builds new array filled with elements that pass a test provided by a function
+.find() returns the values of the first element that passes a test
+*/
