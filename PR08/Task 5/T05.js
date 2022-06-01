1104911699e2da4f8fb6f1b227e0ddacc5b7351c
Antonio <antonio.gaming10@gmail.com>
2022-06-01 17:16:09 +0200

    Pokemonbycolor fixed

--- PR08/Task 5/T05.js
+++ PR08/Task 5/T05.js
@@ -1,6 +1,13 @@
 function pokemonbycol(quest){
     var color = "https://pokeapi.co/api/v2/pokemon-color/" + quest;
-    window.open(color);
+    fetch(color)
+        .then(response => response.json())
+        .then(pokemon => {
+            pokemon.pokemon_species.map((x) => {
+                console.log(x["name"])
+            })
+        })
+        .catch(console.error())
 }
 
 pokemonbycol("yellow")
