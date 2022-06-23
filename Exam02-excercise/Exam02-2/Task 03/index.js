45a595080ddd7897f8b2d2f69ac37225ee34057a
Antonio <antonio.gaming10@gmail.com>
2022-06-23 21:48:22 +0200

    Task 03 fixed

--- Exam02-excercise/Exam02-2/Task 03/index.js
+++ Exam02-excercise/Exam02-2/Task 03/index.js
@@ -1,5 +1,14 @@
 /*
-u JS-u funkcije su objekti te se mogu kreirati pomocu konstruktora Function i koristeci new, salju se stringovi unutar koji se nalaze parametri i tijelo funkcije.
+In JS new operator lets developers create an instance of a user-defined object type or of one of the built-in object types that has a constructor function.
 */
 
-let funk = new Function(arguments);
+function Car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+}
+
+const car1 = new Car('Eagle', 'Talon TSi', 1993);
+
+console.log(car1.make);
+// expected output: "Eagle"
