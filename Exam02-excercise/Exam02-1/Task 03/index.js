59d2e1f1c5f585154c32b00b8e579cbeaa396c83
Antonio <antonio.gaming10@gmail.com>
2022-06-23 22:47:38 +0200

    Task 3 E-01

---
+++ Exam02-excercise/Exam02-1/Task 03/index.js
@@ -0,0 +1,9 @@
+/*
+IIFE - Immediately Invoked Function Expression is a JavaScript function that runs as soon as it is defined.
+*/
+
+let sum = (function(a,b){
+    return a + b;
+})(10,20);
+
+console.log(sum);
