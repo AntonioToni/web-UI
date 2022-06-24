b133c04d772a33b5f7ec72d7fda6c67f937247f9
Antonio <antonio.gaming10@gmail.com>
2022-06-24 11:24:56 +0200

    Task 4 E-00 Fixed

--- Exam02-excercise/Exam02-0/Task 04/index.js
+++ Exam02-excercise/Exam02-0/Task 04/index.js
@@ -1,14 +1,24 @@
-function req() {
-    fetch('https://jsonplaceholder.typicode.com/posts/')
-        .then(response => response.json())
-        .then(json => {
-            for (i in json){
-                let title = json[i].title;
-                let body = json[i].body;
-                document.write("<h2>"+title+"</h2>");
-                document.write("<p>"+body+"</p>")
-            }
+const postsContainer = document.getElementById("posts");
+
+function addPosts(data) {
+    data.forEach((post) => {
+        const container = document.createElement("div");
+        container.className = "post";
+
+        const title = document.createElement("h2");
+        title.innerHTML = `${post.title}`
+        container.appendChild(title);
+
+        const body = document.createElement("p");
+        body.innerHTML = `${post.body}`
+        container.appendChild(body);
+        
+        postsContainer.appendChild(container);
     });
 }
-  
-req();
+
+fetch("https://jsonplaceholder.typicode.com/posts/")
+    .then((response) => response.json())
+    .then((data) => {
+        addPosts(data);
+    })
